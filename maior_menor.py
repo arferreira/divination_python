e3a53aa8c9be655db693fb5c11eb020a4b483ee2
Antonio Ricardo <antonioricardoarfs@gmail.com>
2017-10-26 11:27:47 -0300

    Create a function welcome on file

---
+++ maior_menor.py
@@ -0,0 +1,7 @@
+def welcome():
+    print("Bem-vindo ao jogo da adivinhação.")
+    print("\n\n")
+    name = input("Qual o seu nome? ")
+    print("\n\n")
+    print("Começaremos o jogo para você, {}".format(name))
+
